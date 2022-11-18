047a0d1cd51b5a6201203c31eaaf46e1bfd943bd
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-11-17 21:03:25 -0500

    30 - First time using the do while.

--- challenges.js
+++ challenges.js
@@ -962,14 +962,14 @@ Difficulty:  Difficult
 Prompt:
 
 - Write a function called totalTaskTime that accepts two arguments.
-- The first argument is an array of integers referred to as a "queue".  Each integer in the queue represents a "task", more specifically, the amount of time to complete that task.
+- The first argument is an array of integers referred to as a "queue". Each integer in the queue represents a "task", more specifically, the amount of time to complete that task.
 - The second argument is an integer representing the number of CPU "threads" available to process all of the tasks in the queue.
 - The totalTaskTime function should return an integer representing the total time it is going to take to complete all of the tasks in the queue.
 - You may mutate the "queue" array (first argument) if you wish.
 
 Hint:
 
-- Solve it with paper and pencil first.  Look for patterns and generalize.  Pseudocode!
+- Solve it with paper and pencil first. Look for patterns and generalize.  Pseudocode!
 
 Examples:
 
@@ -981,5 +981,20 @@ totalTaskTime( [2, 2, 3, 3, 4, 4], 2 ) //=> 9
 totalTaskTime( [5, 2, 6, 8, 7, 2], 3 ) // => 12
 -----------------------------------------------------------------*/
 // Your solution for 30- here:
+const totalTaskTime = (stack, channels ) => {
+  if (stack.length === 0) return 0
+  let currentStack = []
+  let clock = 0
+
+  do {
+    while (currentStack.length !== channels && stack.length > 0) {
+      currentStack.push(stack.shift())
+    }
+    currentStack = currentStack?.map(x => x-1).filter(x => x > 0)
+    clock++
+  } while (currentStack.length > 0 || stack.length > 0) 
+  
+  return clock
+}
 
 
